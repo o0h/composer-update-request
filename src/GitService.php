8cb9ed777e3b2d0f61e1c94167548a9d8332abfa
Hideki Kinjyo <dev@o0h.in>
2018-04-03 08:48:48 +0900

    wip

--- src/GitService.php
+++ src/GitService.php
@@ -8,9 +8,47 @@
 
 namespace O0h\ComposerUpdateRequest;
 
+use TQ\Git\Repository\Repository;
+use TQ\Vcs\Cli\CallResult;
+use TQ\Vcs\Repository\Transaction;
 use GitWrapper\GitWrapper;
 
 class GitService
 {
+    /** @var Repository */
+    protected $git;
+
+    public function __construct(string $directory)
+    {
+        $this->git = Repository::open($directory);
+    }
+
+    public function createBranch()
+    {
+        $branchName = $this->getBranchName();
+        $this->git->getGit();
+        /** @var $result CallResult */
+        $result = $this->git->getGit()->{'checkout'}($this->git->getRepositoryPath(), [
+            '-b',
+            $branchName
+        ]);
+        $result->assertSuccess('Failed to create new branch. ' . $result->getStdErr());
+
+        return $result;
+    }
+
+    public function commitAll()
+    {
+        $result = $this->git->transactional(function(Transaction $t) {
+            $t->setCommitMsg('the commit is aaa..');
+        });
+
+        return $result;
+    }
+
+    protected function getBranchName()
+    {
+        return sprintf('update-composer-%s', date('Ymdhis'));
+    }
 
 }
---
+++ src/UpdateRequestPlugin.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace O0h\ComposerUpdateRequest;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Script\ScriptEvents;
+use Composer\Script\Event;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Plugin\PluginInterface;
+
+class UpdateRequestPlugin implements PluginInterface, EventSubscriberInterface
+{
+    /** @var Composer */
+    protected $composer;
+
+    /** @var IOInterface */
+    protected $io;
+
+    /** @var array packages before updating */
+    protected $before = [];
+
+    /** @var array packages after updating */
+    protected $after = [];
+
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::PRE_UPDATE_CMD => [
+                'onPreUpdate',
+            ],
+            ScriptEvents::POST_UPDATE_CMD => [
+                'onPostUpdate',
+            ],
+        ];
+    }
+
+    public function onPreUpdate(Event $arg)
+    {
+        $this->before = $this->getLocalPackages();
+    }
+
+    public function onPostUpdate(Event $arg)
+    {
+        $packages = $this->getLocalPackages();
+        $diff = array_diff_assoc($packages, $this->before);
+        if (!$diff) {
+            // return;
+        }
+        $pjRoot = $this->getPjRoot();
+        $git = new GitService($pjRoot);
+        $r = $git->createBranch();
+        $git->commitAll();
+        var_dump($r);
+    }
+
+    protected function getLocalPackages()
+    {
+        $repo = $this->composer->getRepositoryManager()->getLocalRepository();
+        $localPackages = [];
+        foreach ($repo->getPackages() as $package) {
+            $localPackages[$package->getName()] = $package->getVersion();
+        }
+
+        return $localPackages;
+    }
+
+    protected function getPjRoot()
+    {
+        $dir = __DIR__;
+        while (true) {
+            $path = $dir . DIRECTORY_SEPARATOR . '.git';
+            if (file_exists($path) && is_dir($path)) {
+                break;
+            }
+            $dir .= DIRECTORY_SEPARATOR . '..';
+        }
+        $path = dirname(realpath($path)) . DIRECTORY_SEPARATOR;
+
+        return $path;
+    }
+}
